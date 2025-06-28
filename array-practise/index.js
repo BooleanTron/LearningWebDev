959b52b3beb4e3a7a4d930e5cafab9065845e2da
BooleanTron <booleantron@gmail.com>
2025-06-28 15:26:49 +0530

    array-practise done

---
+++ array-practise/index.js
@@ -0,0 +1,16 @@
+let fruits = ["apple", "orange", "banana"];
+
+//fruits.push("coconut");
+//fruits.pop();
+//fruits.unshift("mango");
+//fruits.shift();
+//console.log(fruits);
+//
+
+let numFruits = fruits.length;
+let index = fruits.indexOf("apple");
+
+fruits.sort().reverse();
+for (let fruit of fruits) {
+  console.log(fruit);
+}
