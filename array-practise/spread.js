59a51b8a25e011a97c1544f338c53fc5590ece62
BooleanTron <booleantron@gmail.com>
2025-06-28 15:43:10 +0530

    spread operator worked

---
+++ array-practise/spread.js
@@ -0,0 +1,10 @@
+let numbers = [9, 3, 5, 4, 1, 2, 8];
+let max = Math.max(...numbers);
+console.log(max);
+
+let username = "BooleanTron";
+let letters = [...username];
+console.log(letters);
+
+
+//the SPREAD OPERATOR unpack any array or string - to use it just preceed your array or string with 3 dots like (...numbers)
